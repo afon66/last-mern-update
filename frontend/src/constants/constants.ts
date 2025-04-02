372d42e777fd1c43aeb6a2138ec4082e29e9093d
Afanasi <afanasi909@gmail.com>
2025-04-02 19:03:15 +0300

    Update constants.ts

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://social-network-opal-six.vercel.app' : 'http://localhost:3000'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://server-zeta-jade.vercel.app' : 'http://localhost:3000'
