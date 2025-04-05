e4f1efb7ce55bab34e098a6b0233d6b7437d9f9f
Afanasi <afanasi909@gmail.com>
2025-04-05 10:20:28 +0300

    Update constants.ts

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://hope-it-works.vercel.app/' : 'http://localhost:3000'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://last-mern-update.vercel.app/' : 'http://localhost:3000'
